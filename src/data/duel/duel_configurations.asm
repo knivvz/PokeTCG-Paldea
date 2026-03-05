7780c5a687ce2eef9a3d8ef1efb8b11238710ab1
knivvz <lukas.grassl@gmx.at>
2026-03-05 10:22:34 +0100

    set all duels to 6 prizes

--- src/data/duel/duel_configurations.asm
+++ src/data/duel/duel_configurations.asm
@@ -19,7 +19,7 @@ DeckIDDuelConfigurations:
 	db LIGHTNING_AND_FIRE_DECK_ID ; deck ID
 	db AARON_PIC ; NPC portrait
 	tx AaronNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx TechText ; rank
 	dw NULL ; element
@@ -27,7 +27,7 @@ DeckIDDuelConfigurations:
 	db WATER_AND_FIGHTING_DECK_ID ; deck ID
 	db AARON_PIC ; NPC portrait
 	tx AaronNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx TechText ; rank
 	dw NULL ; element
@@ -35,7 +35,7 @@ DeckIDDuelConfigurations:
 	db GRASS_AND_PSYCHIC_DECK_ID ; deck ID
 	db AARON_PIC ; NPC portrait
 	tx AaronNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx TechText ; rank
 	dw NULL ; element
@@ -155,7 +155,7 @@ DeckIDDuelConfigurations:
 	db INVINCIBLE_RONALD_DECK_ID ; deck ID
 	db RONALD_PIC ; NPC portrait
 	tx RonaldNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_STOP ; theme
 	dw NULL ; rank
 	dw NULL ; element
@@ -171,7 +171,7 @@ DeckIDDuelConfigurations:
 	db MUSCLES_FOR_BRAINS_DECK_ID ; deck ID
 	db CHRIS_PIC ; NPC portrait
 	tx ChrisNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineFightingIconText ; element
@@ -179,7 +179,7 @@ DeckIDDuelConfigurations:
 	db HEATED_BATTLE_DECK_ID ; deck ID
 	db MICHAEL_PIC ; NPC portrait
 	tx MichaelNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineFightingIconText ; element
@@ -187,7 +187,7 @@ DeckIDDuelConfigurations:
 	db LOVE_TO_BATTLE_DECK_ID ; deck ID
 	db JESSICA_PIC ; NPC portrait
 	tx JessicaNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineFightingIconText ; element
@@ -195,7 +195,7 @@ DeckIDDuelConfigurations:
 	db EXCAVATION_DECK_ID ; deck ID
 	db RYAN_PIC ; NPC portrait
 	tx RyanNPCName ; name text ID
-	db PRIZES_3 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineFightingIconText ; element
@@ -203,7 +203,7 @@ DeckIDDuelConfigurations:
 	db BLISTERING_POKEMON_DECK_ID ; deck ID
 	db ANDREW_PIC ; NPC portrait
 	tx AndrewNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineFightingIconText ; element
@@ -211,7 +211,7 @@ DeckIDDuelConfigurations:
 	db HARD_POKEMON_DECK_ID ; deck ID
 	db MATTHEW_PIC ; NPC portrait
 	tx MatthewNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineFightingIconText ; element
@@ -219,7 +219,7 @@ DeckIDDuelConfigurations:
 	db WATERFRONT_POKEMON_DECK_ID ; deck ID
 	db SARA_PIC ; NPC portrait
 	tx SaraNPCName ; name text ID
-	db PRIZES_2 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineWaterIconText ; element
@@ -227,7 +227,7 @@ DeckIDDuelConfigurations:
 	db LONELY_FRIENDS_DECK_ID ; deck ID
 	db AMANDA_PIC ; NPC portrait
 	tx AmandaNPCName ; name text ID
-	db PRIZES_3 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineWaterIconText ; element
@@ -235,7 +235,7 @@ DeckIDDuelConfigurations:
 	db SOUND_OF_THE_WAVES_DECK_ID ; deck ID
 	db JOSHUA_PIC ; NPC portrait
 	tx JoshuaNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineWaterIconText ; element
@@ -243,7 +243,7 @@ DeckIDDuelConfigurations:
 	db PIKACHU_DECK_ID ; deck ID
 	db JENNIFER_PIC ; NPC portrait
 	tx JenniferNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineLightningIconText ; element
@@ -251,7 +251,7 @@ DeckIDDuelConfigurations:
 	db BOOM_BOOM_SELFDESTRUCT_DECK_ID ; deck ID
 	db NICHOLAS_PIC ; NPC portrait
 	tx NicholasNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineLightningIconText ; element
@@ -259,7 +259,7 @@ DeckIDDuelConfigurations:
 	db POWER_GENERATOR_DECK_ID ; deck ID
 	db BRANDON_PIC ; NPC portrait
 	tx BrandonNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineLightningIconText ; element
@@ -267,7 +267,7 @@ DeckIDDuelConfigurations:
 	db ETCETERA_DECK_ID ; deck ID
 	db BRITTANY_PIC ; NPC portrait
 	tx BrittanyNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineGrassIconText ; element
@@ -275,7 +275,7 @@ DeckIDDuelConfigurations:
 	db FLOWER_GARDEN_DECK_ID ; deck ID
 	db KRISTIN_PIC ; NPC portrait
 	tx KristinNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineGrassIconText ; element
@@ -283,7 +283,7 @@ DeckIDDuelConfigurations:
 	db KALEIDOSCOPE_DECK_ID ; deck ID
 	db HEATHER_PIC ; NPC portrait
 	tx HeatherNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineGrassIconText ; element
@@ -291,7 +291,7 @@ DeckIDDuelConfigurations:
 	db GHOST_DECK_ID ; deck ID
 	db ROBERT_PIC ; NPC portrait
 	tx RobertNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachinePsychicIconText ; element
@@ -299,7 +299,7 @@ DeckIDDuelConfigurations:
 	db NAP_TIME_DECK_ID ; deck ID
 	db DANIEL_PIC ; NPC portrait
 	tx DanielNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachinePsychicIconText ; element
@@ -307,7 +307,7 @@ DeckIDDuelConfigurations:
 	db STRANGE_POWER_DECK_ID ; deck ID
 	db STEPHANIE_PIC ; NPC portrait
 	tx StephanieNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachinePsychicIconText ; element
@@ -315,7 +315,7 @@ DeckIDDuelConfigurations:
 	db FLYIN_POKEMON_DECK_ID ; deck ID
 	db JOSEPH_PIC ; NPC portrait
 	tx JosephNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineGrassIconText ; element
@@ -323,7 +323,7 @@ DeckIDDuelConfigurations:
 	db LOVELY_NIDORAN_DECK_ID ; deck ID
 	db DAVID_PIC ; NPC portrait
 	tx DavidNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineGrassIconText ; element
@@ -331,7 +331,7 @@ DeckIDDuelConfigurations:
 	db POISON_DECK_ID ; deck ID
 	db ERIK_PIC ; NPC portrait
 	tx ErikNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineGrassIconText ; element
@@ -339,7 +339,7 @@ DeckIDDuelConfigurations:
 	db ANGER_DECK_ID ; deck ID
 	db JOHN_PIC ; NPC portrait
 	tx JohnNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineFireIconText ; element
@@ -347,7 +347,7 @@ DeckIDDuelConfigurations:
 	db FLAMETHROWER_DECK_ID ; deck ID
 	db ADAM_PIC ; NPC portrait
 	tx AdamNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineFireIconText ; element
@@ -355,7 +355,7 @@ DeckIDDuelConfigurations:
 	db RESHUFFLE_DECK_ID ; deck ID
 	db JONATHAN_PIC ; NPC portrait
 	tx JonathanNPCName ; name text ID
-	db PRIZES_4 ; number of prize cards
+	db PRIZES_6 ; number of prize cards
 	db MUSIC_DUEL_THEME_1 ; theme
 	tx ClubMemberText ; rank
 	tx ChallengeMachineFireIconText ; element
